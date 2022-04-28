2af257a9aca350287363963c4e5e0c6b6953055b
jmarciosilva <jmarciosilva@gmail.com>
2022-04-28 20:26:32 -0300

    Programa exibindo mensagem com interface grafica

---
+++ SejaBemVindoGui/SejaBemVindoGui/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SejaBemVindoGui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox.Show("Seja Bem Vindo a Programação C#");
+        }
+    }
+}
